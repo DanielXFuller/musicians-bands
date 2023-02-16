41a7a9cfba6ffe30ebb664bf4877cb08b444a217
DanielXFuller <danielxfuller@gmail.com>
2023-02-16 11:26:07 +0000

    added associations for band and musicians

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const {Band} = require('./Band')
 const {Musician} = require('./Musician')
 
+Band.hasMany(Musician, { as: 'musicians'});
+Musician.belongsTo(band);
+
 module.exports = {
     Band,
     Musician
