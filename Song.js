7858cac01115d5d6712b51820614361e2b7e9a81
DanielXFuller <danielxfuller@gmail.com>
2023-02-16 14:19:27 +0000

    added song.js file and defined the song model

---
+++ Song.js
@@ -0,0 +1,16 @@
+const { Sequelize, sequelize } = require('../db');
+
+const Song = sequelize.define('Song', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  duration: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+});
+
+module.exports = {
+  Song,
+};
